4ee2763ddd3eaa3f70c34f167c8e495d4afe206d
Suzie-Williams <164332136+Suzie-Williams@users.noreply.github.com>
2024-04-20 18:15:23 -0400

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,14 +1,20 @@
+//JAVASCRIPT FOR DISPLAYING MODAL
+
+//Declaring Variables
 var modalCnt = document.getElementById("modal-cnt");
 var modalImg = document.getElementById("modal-img");
 var modalOverlay = document.getElementById("modal-overlay");
 var closeBtn = document.getElementById("close-btn");
 
+
+//Function for opening Modal on click of image
 function openModal(imgSrc){
 	modalCnt.style.display = "block";
 	modalImg.src = imgSrc;
 	modalOverlay.style.display = "block";
 }
 
+//Function for closing Modal on click of close button
 function closeModal(){
 	modalCnt.style.display = "none";
 	modalOverlay.style.display = "none";
--- quiz.js
+++ quiz.js
@@ -1,17 +1,24 @@
+//JAVASCRIPT FOR QUIZZES
+
+//Declaring Variables
 var quizSection = document.getElementsByClassName("quiz-section");
 var nextBtns = document.getElementsByClassName("next-btn");
 var results = document.getElementById("results");
+var perfectResults = document.getElementById("perfect-results");
 var currentSection = 0;
 var score = 0;
 
-
+//Event listener for each click of the Next buttons
 for (var i = 0; i < quizSection.length; i++){
 	nextBtns[i].addEventListener("click", nextSection);
 }
 
+
+//Check if answer is correct and change question
 function nextSection(){
 	var currentOptions = quizSection[currentSection].getElementsByClassName("quiz-options");
 
+	//check if answer is correct
 	for (var x = 0; x < currentOptions.length; x++){
 		if (currentOptions[x].checked) {
 			if (currentOptions[x].classList.contains("correct")) {
@@ -19,8 +26,14 @@ function nextSection(){
 			}
 		}
 	}
+	//change question
 	quizSection[currentSection].style.display = "none";
 	currentSection++;
 	quizSection[currentSection].style.display = "inline-block";
+	
+	//Display the results
 	results.innerHTML = `Your score is ${score} / ${nextBtns.length}`;
+	if (score == nextBtns.length) {
+		perfectResults.style.display = "initial";
+	}
 }
